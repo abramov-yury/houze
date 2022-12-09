4bd27263f0cd5a6cc01f637fbd2bafc232c1f18d
yury <yury.message@gmail.com>
2022-12-09 18:12:01 +0400

    implement color scheme switching logic

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,42 @@
+const container = document.querySelector(".theme-switcher");
+const controlThemeLight = container.querySelector(".theme-switcher__control[value='light']");
+const controlThemeSystem = container.querySelector(".theme-switcher__control[value='system']");
+const controlThemeDark = container.querySelector(".theme-switcher__control[value='dark']");
+
+const controlThemeLightHandler = () => {
+
+  document.documentElement.dataset.themeName = "light";
+
+};
+
+const controlThemeDarkHandler = () => {
+
+  document.documentElement.dataset.themeName = "dark";
+
+};
+
+const controlThemeSystemHandler = () => {
+
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  if (prefersDarkScheme.matches) {
+
+    document.documentElement.dataset.themeName = "dark";
+
+  } else {
+
+    document.documentElement.dataset.themeName = "light";
+
+  }
+
+};
+
 const init = () => {
-  console.log()
+
+  controlThemeLight.addEventListener("change", controlThemeLightHandler);
+  controlThemeSystem.addEventListener("change", controlThemeSystemHandler);
+  controlThemeDark.addEventListener("change", controlThemeDarkHandler);
+
 };
 
 init();
